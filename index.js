15e9402a073ad70512ba28b9d0cbf32049494be6
Ray <raymundolarasandoval@gmail.com>
2022-06-14 20:33:05 -0500

    Primera Entrada

---
+++ index.js
@@ -0,0 +1,32 @@
+
+	window.sr = ScrollReveal();
+
+		sr.reveal('.navbar', {
+				duration: 1000,
+				origin: 'bottom',
+				distance: '-50px'
+
+
+		});
+
+
+		sr.reveal('#ray', {
+				duration: 1000,
+				origin: 'left',
+				distance: '-50px'
+
+
+		});
+
+		sr.reveal('#uno', {
+				duration: 1500,
+				origin: 'right',
+				distance: '-50px'
+
+
+		});
+
+
+
+
+		
